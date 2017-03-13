fd0f4ed1011f2fb34351f6e046273648a262524c
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-03-14 00:31:08 +0900

    Fetch and save pages of kindle list

--- lib/kindle_manager/client.rb
+++ lib/kindle_manager/client.rb
@@ -1,8 +1,9 @@
 module KindleManager
   class Client
-    attr_accessor :page
+    attr_accessor :page, :store
 
     def initialize(options = {})
+      @debug = options.fetch(:debug, false)
       begin
         @client = AmazonAuth::Client.new
       rescue => e
@@ -14,11 +15,23 @@ module KindleManager
 
     def load_kindle_list
       sign_in
+      setup_file_store
       go_to_kindle_management_page
+      begin
+        load_next_kindle_list
+      rescue => e
+        byebug
+        # retry ?
+        puts e
+      end
     end
 
     def sign_in
       @page = @client.sign_in
+    end
+
+    def setup_file_store
+      @store = KindleManager::FileStore.new(page)
     end
 
     def go_to_kindle_management_page
@@ -29,6 +42,29 @@ module KindleManager
       page
     end
 
+    def load_next_kindle_list
+      wait_for_selector('.contentCount_myx')
+      @current_loop = 0
+      while @current_loop <= 12 # max attempts
+        if has_more_button?
+          debug_print_page
+          @current_loop = 0
+
+          puts "Clicking もっと表示"
+          page.execute_script "window.scrollBy(0,-800)"
+          page.click_on('もっと表示')
+          sleep 1
+          raise('Clicking of more button may have failed') if has_more_button?
+        else
+          puts "Scrolling #{@current_loop}"
+          page.execute_script "window.scrollBy(0,10000)"
+        end
+        sleep 5
+        @current_loop += 1
+      end
+      debug_print_page
+    end
+
     def quit
       page.driver.quit
     end
@@ -37,5 +73,22 @@ module KindleManager
       seconds.times { sleep(1) unless page.first(selector) }
     end
 
+    def has_more_button?
+      page.all('#contentTable_showMore_myx').map(&:text).include?('もっと表示')
+    end
+
+    def loading?
+      page.first('.myx-popover-loading-wrapper').present?
+    end
+
+    def debug_print_page
+      if @debug
+        store.record_page
+        puts Time.current.strftime("%Y-%m-%d %H:%M:%S")
+        puts "Loop: #{@current_loop}"
+        puts page.first('.contentCount_myx').text if page.first('.contentCount_myx')
+        puts
+      end
+    end
   end
 end
