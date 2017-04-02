6285880483f1d82d2d99f9c8aa08da6a4143a011
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-04-02 21:12:15 +0900

    Fix date parsing in us site

--- lib/kindle_manager/list_parser.rb
+++ lib/kindle_manager/list_parser.rb
@@ -27,8 +27,14 @@ module KindleManager
 
       def date
         @_date ||= begin
-          m = @node.css("div[id^='date']").text.match(/\A(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日\z/)
-          Date.new(m[:year].to_i, m[:month].to_i, m[:day].to_i)
+          date_text = @node.css("div[id^='date']").text
+          begin
+            Date.parse(date_text)
+          rescue ArgumentError => e
+            byebug
+            m = date_text.match(/\A(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日\z/)
+            Date.new(m[:year].to_i, m[:month].to_i, m[:day].to_i)
+          end
         end
       end
 
