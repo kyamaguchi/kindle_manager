f50b021f24bc5dab8e6b370aab8a8dfe959a6eb1
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-04-12 12:43:00 +0900

    Remove debug code

--- lib/kindle_manager/list_parser.rb
+++ lib/kindle_manager/list_parser.rb
@@ -31,7 +31,6 @@ module KindleManager
           begin
             Date.parse(date_text)
           rescue ArgumentError => e
-            byebug
             m = date_text.match(/\A(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日\z/)
             Date.new(m[:year].to_i, m[:month].to_i, m[:day].to_i)
           end
