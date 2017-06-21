f14a7344795791f615f2818997724cf5c8d9273e
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-06-21 15:53:43 +0900

    Fix parse_date to parse another Japanese date
    e.g. '月曜日 6月 19, 2017'

--- lib/kindle_manager/parsers/common.rb
+++ lib/kindle_manager/parsers/common.rb
@@ -7,6 +7,7 @@ module KindleManager
           Date.parse(date_text)
         rescue ArgumentError => e
           m = date_text.match(/\A(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日\z/)
+          m = date_text.match(/(?<month>\d{1,2})月\D+(?<day>\d{1,2}),\D+(?<year>\d{4})/) if m.nil?
           Date.new(m[:year].to_i, m[:month].to_i, m[:day].to_i)
         end
       end
