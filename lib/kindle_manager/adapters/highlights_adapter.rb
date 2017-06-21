c865d2f8a3af6c3f9c1ca5bce399e9893ec3e6c4
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-06-21 17:24:19 +0900

    Refactor highlights_adapter

--- lib/kindle_manager/adapters/highlights_adapter.rb
+++ lib/kindle_manager/adapters/highlights_adapter.rb
@@ -2,6 +2,8 @@ module KindleManager
   class HighlightsAdapter < BaseAdapter
     KINDLE_HIGHLIGHT_URL = "https://read.#{AmazonInfo.domain}/kp/notebook"
 
+    attr_accessor :library_ids, :loaded_library_ids
+
     def fetch
       go_to_kindle_highlights_page
       fetch_library_ids
@@ -17,17 +19,18 @@ module KindleManager
     end
 
     def fetch_library_ids
-      @last_scroll_top = check_library_scroll
+      last_scroll_top = check_library_scroll
       20.times do
-        scroll_library_pane(@last_scroll_top + 20000)
+        scroll_library_pane(last_scroll_top + 20000)
         sleep(2)
         new_scroll_top = check_library_scroll
-        break if @last_scroll_top == new_scroll_top
-        @last_scroll_top = new_scroll_top
+        break if last_scroll_top == new_scroll_top
+        last_scroll_top = new_scroll_top
       end
       snapshot_page
-      @library_ids = doc.css('#library #kp-notebook-library > .a-row').map{|e| e['id'] }
-      log "Number of library ids is #{@library_ids.size}"
+      self.library_ids = doc.css('#library #kp-notebook-library > .a-row').map{|e| e['id'] }
+      self.loaded_library_ids ||= []
+      log "Number of library ids is #{library_ids.size}"
     end
 
     def check_library_scroll
@@ -43,29 +46,42 @@ module KindleManager
     end
 
     def fetch_kindle_highlights
-      @loaded_library_ids ||= []
-      @library_ids.each do |library_id|
-        next if @loaded_library_ids.include?(library_id)
-        log "Loading highlights for #{library_id}"
-        session.first("##{library_id}").click
-        wait_for_selector('#annotations .kp-notebook-annotation-container', wait_time: 10)
-        @title = doc.css('#annotations .kp-notebook-annotation-container h3.kp-notebook-metadata').try!(:text)
-        10.times do
-          sleep(1)
-          @highlights_count = doc.css('#annotations .kp-notebook-annotation-container #kp-notebook-highlights-count').try!(:text)
-          @notes_count = doc.css('#annotations .kp-notebook-annotation-container #kp-notebook-notes-count').try!(:text)
-          break if @highlights_count != '--' && @notes_count != '--'
-        end
-        snapshot_page("Saving page for [#{@title}] (#{library_id}) highlights:#{@highlights_count} notes:#{@notes_count}")
-        if @title.present?
-          @loaded_library_ids << library_id
-        else
-          log "[ERROR] Failed to load #{library_id} or this book doesn't have any highlights and notes"
-        end
+      library_ids.each do |library_id|
+        next if loaded_library_ids.include?(library_id)
+        fetch_book_with_highlights(library_id)
       end
-      failed_ids = @library_ids - @loaded_library_ids
-      log("May have failed with #{failed_ids.inspect}. Retry with client.adapter.session.first('#B000000000').click") if failed_ids.size > 0
+      report_failed_ids
       snapshot_page
+    end
+
+    def fetch_book_with_highlights(library_id)
+      log "Fetching highlights for the book #{library_id}"
+      session.first("##{library_id}").click
+      wait_for_selector('#annotations .kp-notebook-annotation-container', wait_time: 10)
+      title = doc.css('#annotations .kp-notebook-annotation-container h3.kp-notebook-metadata').try!(:text)
+      highlights_count, notes_count = fetch_highlights_and_notes
+      snapshot_page("Saving page for [#{title}] (#{library_id}) highlights:#{highlights_count} notes:#{notes_count}")
+      if title.present?
+        self.loaded_library_ids << library_id
+      else
+        log "[ERROR] Failed to load #{library_id} or this book doesn't have any highlights and notes"
+      end
+    end
+
+    def fetch_highlights_and_notes
+      highlights_count = notes_count = nil
+      10.times do
+        sleep(1)
+        highlights_count = doc.css('#annotations .kp-notebook-annotation-container #kp-notebook-highlights-count').try!(:text)
+        notes_count = doc.css('#annotations .kp-notebook-annotation-container #kp-notebook-notes-count').try!(:text)
+        break if highlights_count != '--' && notes_count != '--'
+      end
+      [highlights_count, notes_count]
+    end
+
+    def report_failed_ids
+      failed_ids = library_ids - loaded_library_ids
+      log("May have failed with #{failed_ids.inspect}. Retry with client.adapter.session.first('#B000000000').click") if failed_ids.size > 0
     end
 
     def load
