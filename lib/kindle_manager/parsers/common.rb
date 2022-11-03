a0e198f404b4f6e42b03be05c9c280383d891851
Kazuho Yamaguchi <kzh.yap@gmail.com>
2022-11-04 00:50:37 +0900

    Fix parsing of date from Japanese text

--- lib/kindle_manager/parsers/common.rb
+++ lib/kindle_manager/parsers/common.rb
@@ -11,7 +11,7 @@ module KindleManager
         begin
           Date.parse(date_text)
         rescue ArgumentError => e
-          m = date_text.match(/\A(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日/)
+          m = date_text.match(/(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日/)
           m = date_text.match(/(?<month>\d{1,2})月\D+(?<day>\d{1,2}),\D+(?<year>\d{4})/) if m.nil?
           raise("Failed to parse date [#{date_text}]") if m.nil?
           Date.new(m[:year].to_i, m[:month].to_i, m[:day].to_i)
