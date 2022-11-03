8fbf8c25b59374be737c7957238befaf716061bd
Kazuho Yamaguchi <kzh.yap@gmail.com>
2022-11-03 19:40:31 +0900

    Fix fetching of books pages

--- lib/kindle_manager/adapters/books_adapter.rb
+++ lib/kindle_manager/adapters/books_adapter.rb
@@ -10,7 +10,7 @@ module KindleManager
         puts "[ERROR] #{e}"
         puts e.backtrace
         puts
-        puts "Retry manually -> client.adapter.load_next_kindle_list or client.session etc."
+        puts "Investigate the error using 'client.session', 'client.doc' etc."
       end
     end
 
@@ -18,9 +18,9 @@ module KindleManager
       log "Visiting kindle management page"
       3.times do
         session.visit URL_FOR_KINDLE_CONTENTS
-        wait_for_selector('.navHeader_title_myx')
-        if session.has_css?('.navHeader_title_myx')
-          log "Page found '#{session.first('.navHeader_title_myx').text}'"
+        wait_for_selector('#content-page-title')
+        if session.has_css?('#content-page-title')
+          log "Page found '#{session.first('#content-page-title').text}'"
           break
         else
           submit_signin_form
@@ -29,34 +29,15 @@ module KindleManager
     end
 
     def load_next_kindle_list
-      wait_for_selector('.contentCount_myx')
-      current_loop = 0
-      last_page_scroll_offset = page_scroll_offset
-      while current_loop <= max_scroll_attempts
-        break if limit && limit < number_of_fetched_books
-        if has_more_button?
-          snapshot_page
-          current_loop = 0
-
-          log "Clicking 'Show More'"
-          session.execute_script "window.scrollBy(0,-800)"
-          show_more_button.click
-          sleep fetching_interval
-        else
-          log "Loading books with scrolling #{current_loop+1}"
-          session.execute_script "window.scrollBy(0,10000)"
-        end
+      wait_for_selector('#CONTENT_COUNT')
+      loop do
+        snapshot_page
+        break if current_page == max_page
+        break if limit && limit <= number_of_fetched_books
+        session.first("#page-#{current_page + 1}").click
         sleep fetching_interval
-        if last_page_scroll_offset == page_scroll_offset
-          log "Stopping loading because 'page_scroll_offset' didn't change after a loop"
-          break
-        else
-          debug "last_page_scroll_offset:#{last_page_scroll_offset} new page_scroll_offset:#{page_scroll_offset}"
-        end
-        last_page_scroll_offset = page_scroll_offset
-        current_loop += 1
       end
-      log "Stopped loading. You may want to resume with 'client.adapter.load_next_kindle_list'"
+      log "Stopped loading. You may want to continue with 'client.session', 'client.doc' etc."
       snapshot_page
     end
 
@@ -69,33 +50,26 @@ module KindleManager
       books.sort_by{|b| [-b.date.to_time.to_i, -b.fetched_at.to_i] }.uniq(&:asin)
     end
 
-    def page_scroll_offset
-      session.evaluate_script('window.pageYOffset')
+    def current_page
+      doc.css('#pagination a.active').first.text.to_i
     end
 
-    def has_more_button?
-      !!show_more_button
-    end
-
-    def show_more_button
-      session.all('.contentTableShowMore_myx').find{|e| e['outerHTML'].match(/cnt_shw_more/) }
+    def max_page
+      @_max_page ||= doc.css('#pagination a').last.text.to_i
     end
 
     def number_of_fetched_books
-      re = (AmazonInfo.domain =~ /\.jp\z/ ? /(\d+)〜(\d+)/ : /(\d+) - (\d+)/)
-      wait_for_selector('.contentCount_myx')
-      text = doc.css('.contentCount_myx').text
+      re = (AmazonInfo.domain =~ /\.jp\z/ ? /(\d+)から(\d+)/ : /(\d+) - (\d+)/)
+      wait_for_selector('#CONTENT_COUNT')
+      text = doc.css('#CONTENT_COUNT').text
+      log "Number of books: [#{text}]"
       m = text.match(re)
       return m[2].to_i if m.present?
       raise("Couldn't get the number of fetched books [#{text}]")
     end
 
-    def loading?
-      session.first('.myx-popover-loading-wrapper').present?
-    end
-
     def snapshot_page
-      if (text = doc.css('.contentCount_myx').try!(:text)).present?
+      if (text = doc.css('#CONTENT_COUNT').try!(:text)).present?
         log "Current page [#{text.to_s.gsub(/[[:space:]]+/, ' ').strip}]"
       end
       store.record_page
