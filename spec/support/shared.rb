29157debc3d9e6bfaad86676ee97122c46eb80b8
Kazuho Yamaguchi <kzh.yap@gmail.com>
2023-07-24 00:39:56 +0900

    Fix error with ruby 3.2

--- spec/support/shared.rb
+++ spec/support/shared.rb
@@ -1,5 +1,5 @@
 def find_fixture_filepath(name)
   path = File.join('spec', 'fixtures', 'files', name)
-  pending("Put your html in #{path} for testing") unless File.exists?(path)
+  pending("Put your html in #{path} for testing") unless File.exist?(path)
   path
 end
